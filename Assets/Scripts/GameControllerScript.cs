b7a1ece99870f224244f0e73e6328356fdd48440
nixiesquid <audu817@gmail.com>
2019-09-15 18:56:57 +0900

    implement GameOver

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameControllerScript : MonoBehaviour
 {
     private int score;
     public Text scoreText;
     public Text replayText;
+
     public GameObject enemy;
+
+    private bool isGameOver;
 
 
     IEnumerator SpawnEnemy()
@@ -32,12 +36,16 @@ public class GameControllerScript : MonoBehaviour
         score = 0;
         UpdateScoreText();
         replayText.text = "";
+        isGameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && isGameOver)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
     }
 
     public void AddScore(int scoreToAdd)
@@ -54,5 +62,6 @@ public class GameControllerScript : MonoBehaviour
     public void GameOver()
     {
         replayText.text = "Hit SPACE to replay!";
+        isGameOver = true;
     }
 }
